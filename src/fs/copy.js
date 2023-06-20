be67bf8c2b62f772896378345603db5600f0a77f
Serg Kobrusev <skobrusev446@gmail.com>
2023-06-21 00:06:35 +0300

    feat: add copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,20 +1,31 @@
 import * as fs from 'node:fs/promises';
+import path from 'node:path';
+import { promisify } from 'node:util';
 
 const copy = async () => {
-  // var fs = require('fs');
-  // const path = require('path');
-  // const path1 = path.format({
-  //   dir: './files',
-  //   base: 'fresh.txt',
-  // });
 
-  fs.writeFile('./src/fs/files/fresh.txt', 
-  'I am fresh and young', 
-  function(error){
-  if(error) throw error; 
-  console.log('FS operation failed');
-  });
 
+    fs.mkdir( path.join( "./src/fs", "files-copy"), ( err ) => {
+       if ( err ) { return console.error( err ); };
+    });
+   
+    let folderName = "./src/fs/files";
+    
+    let folderNameNew = "./src/fs/files-copy";
+
+    fs.readdir( folderName, { withFileTypes: true }, 
+      (err, files) => {
+        if ( err ) { throw err; };
+        files.forEach( file => {
+        fs.copyFile( folderName + `/${file.name}`, folderNameNew + `/${file.name}` , 
+        (err) => { if ( err ) { console.log( 'FS operation failed' , err ); } });
+        }
+        ) ;
+    
+      }
+    ) ;
 };
 
 await copy();
+
+
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import * as fs from 'node:fs/promises';
+
 const create = async () => {
-    // Write your code here 
+  // var fs = require('fs');
+  // const path = require('path');
+  // const path1 = path.format({
+  //   dir: './files',
+  //   base: 'fresh.txt',
+  // });
+
+  fs.writeFile('./src/fs/files/fresh.txt', 
+  'I am fresh and young', 
+  function(error){
+  if(error) throw error; 
+  console.log('FS operation failed');
+  });
 };
 
 await create();
