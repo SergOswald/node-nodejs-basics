46923d238b797601347d889f5cfe249857b78378
SergOswald <skobrusev446@gmail.com>
2023-06-29 10:13:54 +0300

    feat: correct copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -4,7 +4,7 @@ import { promisify } from 'node:util';
 
 const copy = async () => {
 
-
+/*
     fs.mkdir( path.join( "./src/fs", "files-copy"), ( err ) => {
        if ( err ) { return console.error( err ); };
     });
@@ -24,6 +24,44 @@ const copy = async () => {
     
       }
     ) ;
+
+    */
+
+    //let folderName = "./src/fs/files";
+    
+    //let folderNameNew = "./src/fs/files-copy";
+
+ //   fs.mkdir( path.join( "./src/fs", "files-copy"), ( err ) => {
+
+ //path.join(__dirname, 'test')
+
+ fs.mkdir(  path.join("./src/fs", "files-copy"), ( err ) => {
+
+ //   fs.mkdir( "./src/fs", "files-copy", ( err ) => {
+
+      if (err) {
+        throw err;
+      } else {
+        console.log("111");
+        fs.readdir( "./src/fs/files", { withFileTypes: true }, 
+          (err, files) => {
+            if ( err ) { 
+              throw err; 
+            }else{
+
+            files.forEach( file => {
+            fs.copyFile( "./src/fs/files" + `/${file.name}`, "./src/fs/files-copy" + `/${file.name}` , 
+            (err) => { if ( err ) { console.log( 'FS operation failed' , err ); } });
+            }
+            ) ;
+          };
+          }
+        ) ;
+
+      }
+
+    });
+
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,13 +1,6 @@
 import * as fs from 'node:fs/promises';
 
 const create = async () => {
-  // var fs = require('fs');
-  // const path = require('path');
-  // const path1 = path.format({
-  //   dir: './files',
-  //   base: 'fresh.txt',
-  // });
-
   fs.writeFile('./src/fs/files/fresh.txt', 
   'I am fresh and young', 
   function(error){
