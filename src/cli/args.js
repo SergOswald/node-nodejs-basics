032f5729d08f4fbe9bbf1f9040addf2335597df7
SergOswald <skobrusev446@gmail.com>
2023-07-03 19:43:40 +0300

    feat: add args

--- src/cli/args.js
+++ src/cli/args.js
@@ -2,8 +2,8 @@ import { argv } from "node:process";
 
 const parseArgs = () => {
   const [...args] = argv;
-  for( let i=0; i < args.length; i=i + 2) {
-    console.log( args[ i ] + " is " + args[ i + 1 ] );
+  for(let i=0; i<args.length; i=i+2) {
+    console.log(args[i] + " is " + args[i+1]);
   };
 };
 
