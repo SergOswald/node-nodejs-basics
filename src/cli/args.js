2ef94f54d0c0f424c3722d628300e592a3c44658
SergOswald <skobrusev446@gmail.com>
2023-07-03 19:40:58 +0300

    feat: add args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,10 @@
+import { argv } from "node:process";
+
 const parseArgs = () => {
-    // Write your code here 
+  const [...args] = argv;
+  for( let i=0; i < args.length; i=i + 2) {
+    console.log( args[ i ] + " is " + args[ i + 1 ] );
+  };
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,8 @@
 const parseEnv = () => {
-    // Write your code here 
+const RSS_name1 = process.env.RSS_foo;
+const RSS_name2 = process.env.RSS_bar;
+console.log("RSS_name1 =",`${RSS_name1}`);
+console.log("RSS_name2 =",`${RSS_name2}`);
 };
 
 parseEnv();
