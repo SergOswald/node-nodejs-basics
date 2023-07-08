3cc2fae38e06c827d5e22b3762b3d9f6fdb92ca4
SergOswald <skobrusev446@gmail.com>
2023-07-08 18:58:01 +0300

    feat: add streams

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,14 @@
+//npm run streams:transform
+
+import * as fs from "node:fs";
+
 const transform = async () => {
-    // Write your code here 
+    let myReadStream = fs.createReadStream("./src/streams/files/process.stdin", "UTF8");
+    let myWriteStream = fs.createWriteStream("./src/streams/files/process.stdout", "UTF8");
+    myReadStream.on('data', function (chunk) {
+      myWriteStream.write(chunk.toString().split('').reverse().join(''));
+    });
+
 };
 
 await transform();
