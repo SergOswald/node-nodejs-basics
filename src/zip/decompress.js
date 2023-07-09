25771f1bac2033134ad2a5a318927d3b8a933f61
SergOswald <skobrusev446@gmail.com>
2023-07-09 14:36:59 +0300

    feat: add zip

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,17 @@
+//npm run zip:decompress
+
+import * as fs from "node:fs";
+import zlib from "node:zlib";
+import { pipeline } from "node:stream";
+import { createGzip } from "node:zlib";
+import { createUnzip } from "node:zlib";
+
 const decompress = async () => {
-    // Write your code here 
+    let myReadStream = fs.createReadStream("./src/zip/files/archive.gz");
+    let myWriteStream = fs.createWriteStream("./src/zip/files/fileToCompress.txt");
+    const unzip = zlib.createUnzip();
+    myReadStream.pipe(unzip).pipe(myWriteStream);
+
 };
 
 await decompress();
